347417f8bba0b1769d2a95705003202724836281
Kenny-WilliamsStockdale <68571101+Kenny-WilliamsStockdale@users.noreply.github.com>
2022-05-25 23:44:33 +1200

    added project files to repo

---
+++ DAT602-Milestone-2-Kenny/DAT602 MS2 Console App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAT602_MS2_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccess aDataAccess = new DataAccess();
+
+            Console.WriteLine("Message is " + aDataAccess.Register("Player3", "P@ssword2", "player3@mail.com", "0"));
+            Console.WriteLine("Message is " + aDataAccess.Login("Player", "P@ssword2"));
+            Console.WriteLine("Message is " + aDataAccess.Logout("Player"));
+            Console.WriteLine("Message is " + aDataAccess.deleteAccount("Player3"));
+
+        }
+    }
+}
